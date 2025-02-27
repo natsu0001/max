c3a647b4947682183444ffd17348d5336d39b426
natsu0001 <talukdarabhishek687@gmail.com>
2025-02-27 16:31:56 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import New from './pages/new/New';
 import Single from './pages/single/Single';
+import Login from './pages/login/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      <List/>
-      <New/>
-      <Single/>
-    </div>
+   <div> <Router>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/list" element={<List />} />
+      <Route path="/new" element={<New />} /> 
+      <Route path="/single" element={<Single />} />
+    </Routes>
+  </Router>
+  </div>
+  
   );
 }
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import './login.scss'
 
 const Login = () => {
   return (
-    <div className='Login'>
+    <div className='login'>
         <h1>Login</h1>
     </div>
   )
